53e9282f67d951a68fd248e0b20f6745c6825fa3
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-01-20 16:23:27 -0300

    delete item

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
+import { FaTimes } from 'react-icons/fa';
 import propTypes from 'prop-types';
 import Card from './shared/Card';
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={() => handleDelete(item.id)}>
+        <FaTimes color="purple"></FaTimes>
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,14 @@
 import propTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback!</p>;
   }
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
