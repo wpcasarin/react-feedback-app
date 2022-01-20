4e513e036d73305cb2b81a88a64c0bb16a9f7906
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-01-20 13:36:13 -0300

    feedback item/data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+import FeedbackData from './data/FeedBackData';
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
       <div className="container">
-        <h1>My App</h1>
+        <FeedbackItem />
       </div>
     </>
   );
